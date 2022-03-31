9a31ef888af2046b5fc93c4d14936b49cb3de8c9
Anupam Dahal <anupamdahal1@gmail.com>
2022-03-31 16:16:21 -0500

    "Implemented grpc server in simulator"

---
+++ simulator/src/main/java/simulator/Simulator.java
@@ -0,0 +1,17 @@
+package simulator;
+
+public class Simulator{
+  public Simulator(){
+
+  }
+
+  public void init(){
+
+  }
+
+  public void run(){
+    System.out.println("Running the Server");
+  }
+
+
+}
