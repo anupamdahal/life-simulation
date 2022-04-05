f3d9f0d8f5d813f0b14783af864917ed33928d23
Ethan Orr <ejo0003@uah.edu>
2022-04-04 22:12:22 -0500

    added methods to obstacle

--- simulator/src/main/java/simulator/entity/Obstacle.java
+++ simulator/src/main/java/simulator/entity/Obstacle.java
@@ -1,12 +1,35 @@
 package simulator.entity;
 
 public class Obstacle extends Entity {
-    private double x, y, width, height;
+    private double x, y, diameter, height;
 
-    public Obstacle(double x, double y, double width, double height) {
+    public Obstacle(double x, double y, double diameter, double height) {
         this.x = x;
         this.y = y;
-        this.width = width;
+        this.diameter = diameter;
         this.height = height;
+    }
+
+    // TODO: is this obstacle in the path of a planned movement?
+    // for now, is the point something is trying to move to inside an obstacle
+    public boolean isBlocking(int x1, int y1) {
+        double dx = x - x1;
+        double dy = y - y1;
+        double dist = Math.sqrt(Math.pow(dx,2) + Math.pow(dy,2));
+
+        if (dist < diameter/2.0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
     }
 }
