dd8faa8f3b810d26f77c01aa3b9dc20dc84dd2dd
Ethan Orr <ejo0003@uah.edu>
2022-04-06 00:09:07 -0500

    calculate genes for offspring of two predators

--- simulator/src/main/java/simulator/entity/Predator.java
+++ simulator/src/main/java/simulator/entity/Predator.java
@@ -1,9 +1,12 @@
 package simulator.entity;
 
+import java.util.Random;
+
 public class Predator extends Animal {
     private PredatorConfig predatorConfig;
     private int x, y;
-    private String genotype;
+
+    private static Random rand = new Random();
 
     public Predator(int x, int y, int energy, String genotype)
     {
@@ -13,6 +16,32 @@ public class Predator extends Animal {
         this.energy = energy;
         this.genotype = genotype;
         type = EntityType.PREDATOR;
+    }
+
+    @Override
+    public boolean update() {
+
+        return true;
+    }
+
+    private String mixGenes(Predator partner) {
+        String offspringGenes = "";
+        // randomly select either allele of each gene from each parent
+        for (int i = 0; i < 7; i+=3) {
+            offspringGenes += this.genotype.charAt(i + rand.nextInt(2));
+            offspringGenes += partner.genotype.charAt(i + rand.nextInt(2));
+            offspringGenes += " ";
+        }
+        // reverse aA sS fF cases
+        offspringGenes.replace("aA", "Aa");
+        offspringGenes.replace("sS", "Ss");
+        offspringGenes.replace("fF", "Ff");
+
+        return offspringGenes;
+    }
+
+    public String getGenotype() {
+        return genotype;
     }
 
     public int getX() {
