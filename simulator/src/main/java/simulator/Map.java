483aed8f61d3ccff698a721b986e32651b97a0eb
Ethan Orr <ejo0003@uah.edu>
2022-04-05 20:52:12 -0500

    added methods to set map width and height - additionally, added function isPointInBounds()

--- simulator/src/main/java/simulator/Map.java
+++ simulator/src/main/java/simulator/Map.java
@@ -1,10 +1,12 @@
 package simulator;
 
 import java.util.*;
+import javafx.util.Pair;
 import simulator.entity.*;
 
 public class Map {
     public ArrayList<Entity> entities;
+    private int width, height;
 
     // there is only one map, so this is a singleton
     private static Map theInstance = new Map();
@@ -15,5 +17,51 @@ public class Map {
 
     public Map getInstance() {
         return theInstance;
+    }
+
+    // return true if a given point is within the map confines
+    public boolean isPointInBounds(int x, int y) {
+        if (0 < x && x < this.width && 0 < y && y < this.height) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    // seach the map for entities of a given type within a range
+    // of a source entity
+    // return a list of range, object pairs
+    // TODO: make this more efficient
+    public ArrayList<Pair<Double, Entity>> search(Entity source, Entity.EntityType type, int range) {
+        ArrayList<Pair<Double, Entity>> result = new ArrayList<Pair<Double, Entity>>();
+        for (int i=0; i < entities.size(); i++) {
+            if (entities.get(i).type != type) {
+                continue;
+            }
+            Entity target = entities.get(i);
+            double distance = Math.sqrt(Math.pow(source.getX() - target.getX(), 2)
+                + Math.pow(source.getY() - target.getY(), 2));
+            if (distance <= range) {
+                result.add(new Pair<>(distance, target));
+            }
+        }
+        return result;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return this.width;
+    }
+
+    public int getHeight() {
+        return this.height;
     }
 }
