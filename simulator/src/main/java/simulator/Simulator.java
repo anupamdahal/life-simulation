029e974f393c74ed322090b4075a0c7f7947cf5a
Ethan Orr <ejo0003@uah.edu>
2022-04-14 20:48:14 -0500

    actually initialized empty cells to 0

--- simulator/src/main/java/simulator/Simulator.java
+++ simulator/src/main/java/simulator/Simulator.java
@@ -115,6 +115,12 @@ public class Simulator{
     // TODO: send a snapshot of the simulation back to the server
     int[][] frame = new int[map.getWidth()][map.getHeight()];
     // fill the frame with positions of entities
+    // initialize all values to 0
+    for (int x=0; x < map.getWidth(); x++) {
+      for (int y=0; y < map.getHeight(); y++) {
+        frame[x][y] = 0;
+      }
+    }
     // TODO: fill width of plants and obstacles
     // TODO: ensure that grazers/predators take precedence over plants
     for (int i=0; i < map.entities.size(); i++) {
